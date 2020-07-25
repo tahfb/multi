083c746f875edcddd5e77e24b3b9a226acda7312
tahfb <68764143+tahfb@users.noreply.github.com>
2020-07-25 05:24:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update
+apt-get update
 git clone https://github.com/Ha3MrX/Gemail-Hack.git && mv Gemail-Hack gmail
 git clone https://github.com/umeshshinde19/instainsane && mv instainsane instagram
 git clone https://github.com/NecktieRed/Brute-force-Twitter && mv Brute-force-Twitter twitter
