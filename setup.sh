b5004ef9b22c7aea3c15715b6c7e7b04cbdca69f
tahfb <68764143+tahfb@users.noreply.github.com>
2020-07-25 05:11:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ git clone https://github.com/Ha3MrX/Gemail-Hack.git && mv Gemail-Hack gmail
 git clone https://github.com/umeshshinde19/instainsane && mv instainsane instagram
 git clone https://github.com/NecktieRed/Brute-force-Twitter && mv Brute-force-Twitter twitter
 apt install python-pip
+pip install selenium==2.53.6 && pip install termcolor
+cd instagram 
+bash install.sh
+cd ..
+pkg install git perl nmap python python3 python-pip python3-pip
+git clone https://github.com/sullo/nikto.git 
