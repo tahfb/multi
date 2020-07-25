e324862cee77f3fbeac64a64454a24f5b504ba2e
tahfb <68764143+tahfb@users.noreply.github.com>
2020-07-25 05:31:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 apt-get update
 git clone https://github.com/Ha3MrX/Gemail-Hack.git && mv Gemail-Hack gmail
+cd gmail
+chmod +x gemailhack.py
+cd ..
 git clone https://github.com/umeshshinde19/instainsane && mv instainsane instagram
 git clone https://github.com/NecktieRed/Brute-force-Twitter && mv Brute-force-Twitter twitter
 apt install python-pip
